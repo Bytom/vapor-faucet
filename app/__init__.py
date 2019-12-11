762dc66bb5bb9c33acbfb0b537640a8ffc99794b
Chengcheng Zhang <943420582@qq.com>
2019-12-11 18:44:13 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app import api
 from app.config import config
-# from flask_cors import CORS
+from flask_cors import CORS
 
 
 
@@ -10,7 +10,7 @@ def create_app(config_name=None):
     """
     app = Flask('app')
 
-    # CORS(app)
+    CORS(app)
     configure_app(app, config_name)
     configure_extensions(app)
     register_blueprints(app)
